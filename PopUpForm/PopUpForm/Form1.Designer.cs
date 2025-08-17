d33146c084ddfe305b0b7c928f5effc6d2c5c021
Brandon0230 <103169140+Brandon0230@users.noreply.github.com>
2025-08-17 14:26:08 +1000

    Add files via upload

---
+++ PopUpForm/PopUpForm/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace PopUpForm
+{
+    partial class PopUpForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.popupBtn = new System.Windows.Forms.Button();
+            this.popupLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // popupBtn
+            // 
+            this.popupBtn.Location = new System.Drawing.Point(619, 315);
+            this.popupBtn.Name = "popupBtn";
+            this.popupBtn.Size = new System.Drawing.Size(169, 73);
+            this.popupBtn.TabIndex = 0;
+            this.popupBtn.Text = "PopUp";
+            this.popupBtn.UseVisualStyleBackColor = true;
+            this.popupBtn.Click += new System.EventHandler(this.popupBtn_Click);
+            // 
+            // popupLbl
+            // 
+            this.popupLbl.AutoSize = true;
+            this.popupLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.popupLbl.Location = new System.Drawing.Point(326, 9);
+            this.popupLbl.Name = "popupLbl";
+            this.popupLbl.Size = new System.Drawing.Size(133, 42);
+            this.popupLbl.TabIndex = 1;
+            this.popupLbl.Text = "PopUp";
+            this.popupLbl.Click += new System.EventHandler(this.popupLbl_Click);
+            // 
+            // PopUpForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.popupLbl);
+            this.Controls.Add(this.popupBtn);
+            this.Name = "PopUpForm";
+            this.Text = "Pop Up Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button popupBtn;
+        private System.Windows.Forms.Label popupLbl;
+    }
+}
+
---
+++ PopUpForm/PopUpForm/Form1.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PopUpForm
+{
+    public partial class PopUpForm : Form
+    {
+        //Creates a Random Obj to Generate the Random Coordinates for the PopUp Forms
+        private static Random randomCoordinates = new Random();
+        public PopUpForm()
+        {
+            InitializeComponent();
+        }
+            public int i = 1;
+        private void popupBtn_Click(object sender, EventArgs e)
+        {
+            //Makes 3 Random PopUp Forms on a Single Button Click
+            for (int j = 0; j < 3; j++)
+            {
+                Form PopUp = new Form();
+                ShowPopUp(PopUp);
+            }
+        }
+        private void ShowPopUp(Form PopUpForm)
+        {
+            //Set the Primary Screen's Working Area
+            Rectangle workingArea = Screen.PrimaryScreen.WorkingArea;
+            //set the maximum X and Y coordinates
+            int maxX = workingArea.Width - this.Width;
+            int maxY = workingArea.Height - this.Height;
+            //set Random coordinates within the working area
+            int randomX = randomCoordinates.Next(0, maxX);
+            int randomY = randomCoordinates.Next(0, maxY);
+            //Set PopUp Form Properties
+            PopUpForm.Text = $"PopUp Form {i}";
+            PopUpForm.Name = $"PopUp Form {i}";
+            PopUpForm.Controls.Add(new Label
+            {
+                Text = $"PopUp Detected Number {i}",
+                AutoSize = true,
+                Location = new Point(10, 10)
+            });
+            //Set the PopUp Form Location
+            PopUpForm.StartPosition = FormStartPosition.Manual;
+            PopUpForm.Location = new Point(randomX, randomY);
+            //Show the PopUp Form
+            PopUpForm.Show();
+            i++;
+        }
+
+        private void popupLbl_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
