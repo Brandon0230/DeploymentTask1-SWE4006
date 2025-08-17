fe94259e9fd035bdc77fcb532cd424cec84d717e
Brandon0230 <103169140+Brandon0230@users.noreply.github.com>
2025-08-17 14:25:39 +1000

    Add files via upload

---
+++ UnitConverter/Convert Units.cs
@@ -0,0 +1,12 @@
+﻿
+namespace UnitConverter
+{
+    public class Convert_Units
+    {
+        public double Convert(double length, LengthConverter fromConverter, LengthConverter toConverter)
+        {
+            double lengthInMeters = fromConverter.ConvertToMetre(length);
+            return toConverter.ConvertFromMetre(lengthInMeters);
+        }
+    }
+}
---
+++ UnitConverter/LengthConverter.cs
@@ -0,0 +1,10 @@
+﻿
+namespace UnitConverter
+{
+    public abstract class LengthConverter
+    {
+        public abstract string Name { get; }
+        public abstract double ConvertToMetre(double length);
+        public abstract double ConvertFromMetre(double length);
+    }
+}
